bd5d62375bf1b19105f44a696bf147b5742a05e2
greydevv <greysonmurray.dev@gmail.com>
2023-01-29 16:07:30 -0500

    Build initial compilation interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::error::Error;
+use std::time::Instant;
+
+use colored::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next(); // skip executable name
+
+    let now = Instant::now();
+    let status_string = match compile(args) {
+        Ok(()) => "SUCCESS".green(),
+        Err(e) => {
+            eprintln!("\n{}", e);
+            "FAILURE".red()
+        }
+    }.bold();
+    let elapsed = now.elapsed();
+    eprintln!("\n[{}, took {:.2}s]", status_string, elapsed.as_secs_f32());
+}
+
+fn compile(mut _args: impl Iterator<Item = String>) -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
