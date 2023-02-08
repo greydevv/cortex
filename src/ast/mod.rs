827b5bb478f7beaab40168339569d075cfc2e28f
greydevv <greysonmurray.dev@gmail.com>
2023-02-08 14:53:46 -0500

    Implement basic constant folding prototype

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -32,4 +32,26 @@ impl AstNode {
 
         format!("{}{}{}", "  ".repeat(indents).to_string(), self, ast_string)
     }
+
+    // pub fn fold(&self) -> Option<AstNode> {
+    //     match &self {
+    //         AstNode::BinaryExpr { op, lhs, rhs } => {
+    //             match (&**rhs, &**lhs) {
+    //                 (AstNode::Num(lhs_n), AstNode::Num(rhs_n)) => Some(AstNode::Num(AstNode::fold_const_i32(op.clone(), *lhs_n, *rhs_n))),
+    //                 _ => None
+    //             }
+    //         },
+    //         _ => None
+    //     }
+    // }
+    //
+    // pub fn fold_const_i32(op_kind: OpKind, a: i32, b: i32) -> i32 {
+    //     match op_kind {
+    //         OpKind::Add => a + b,
+    //         OpKind::Sub => a - b,
+    //         OpKind::Mul => a * b,
+    //         OpKind::Div => a / b,
+    //         _ => panic!("Unhandled OpKind in constant folding of i32: {}", op_kind)
+    //     }
+    // }
 }
